7164bdfc3a2f2a95819c1e506eac4a73b9ef83c6
guzhu@fairygui.com <guzhu@fairygui.com>
2019-04-01 10:42:38 +0800

    fix #41

--- libfairygui/Classes/UIPackage.cpp
+++ libfairygui/Classes/UIPackage.cpp
@@ -727,6 +727,7 @@ void UIPackage::loadFont(PackageItem * item)
 
                 if (fontSize == 0)
                     fontSize = bh;
+                lineHeight = MAX(fontSize, lineHeight);
             }
         }
 
